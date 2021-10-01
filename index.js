bc88f799655ce40e5bd0a31046420e207b3509e1
Alex Koenig <alex.koenig4@gmail.com>
2021-10-01 00:06:01 -0400

    Exercise 3.2

--- index.js
+++ index.js
@@ -24,8 +24,13 @@ let persons = [
   }
 ]
 
-app.get('/', (req, res) => {
-  res.send()
+app.get('/info', (req, res) => {
+  const date = new Date();
+  const info = `
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${date}</p>
+  `
+  res.send(info)
 })
 
 app.get('/api/persons', (req, res) => {
