73ebd894a5e5be1f6df1f1c8070a1e910a70b754
Alex Koenig <alex.koenig4@gmail.com>
2021-10-02 13:21:26 -0400

    Exercise 3.9

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 let persons = [
@@ -35,6 +36,7 @@ morgan.token('body', (req, res) => {
   return JSON.stringify(req.body)
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -96,7 +98,7 @@ app.delete('/api/persons/:id', (req, res) => {
   res.status(204).end()
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}...`)
 })
