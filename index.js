f15342b85aa9a91b7dda3a1d48e7de3711326227
Alex Koenig <alex.koenig4@gmail.com>
2021-10-02 17:18:09 -0400

    Exercise 3.15

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ morgan.token('body', (req, res) => {
   return JSON.stringify(req.body)
 })
 
+const errorHandler = (err, req, res, next) => {
+  console.log(err.message);
+
+  if(err.name === 'CastError') {
+    return res.status(400).send({ err: 'Malformatted id' })
+  }
+
+  next(err)
+}
+
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -51,22 +61,27 @@ app.post('/api/persons', (req, res) => {
   })
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find(p => p.id === id);
-  if(person) {
-    res.json(person)
-  } else {
-    res.status(404).end()
-  }
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if(person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(err => next(err))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter(p => p.id !== id);
-
-  res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(person => {
+      res.status(204).end()
+    })
+    .catch(err => next(err))
 })
+
+app.use(errorHandler)
 
 // Start App
 const PORT = process.env.PORT
