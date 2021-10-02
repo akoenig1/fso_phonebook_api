de19e60e6120bf103bc316d4de0319e0650426cd
Alex Koenig <alex.koenig4@gmail.com>
2021-10-02 16:55:10 -0400

    Exercise 3.14

--- index.js
+++ index.js
@@ -5,36 +5,6 @@ const mongoose = require('mongoose')
 const Person = require('./models/person')
 const app = express()
 
-// Local Data
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
-
-const generateId = () => {
-  const max = 100000
-  const id = Math.floor(Math.random() * max)
-  return id
-}
-
 // Middlewares
 morgan.token('body', (req, res) => {
   return JSON.stringify(req.body)
@@ -44,7 +14,6 @@ app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.static('build'))
-
 
 // Routes
 app.get('/info', (req, res) => {
@@ -72,23 +41,14 @@ app.post('/api/persons', (req, res) => {
     })
   }
 
-  console.log(persons.filter(p => p.name === body.name));
-
-  if(persons.filter(p => p.name === body.name).length > 0) {
-    return res.status(400).json({
-      error: 'Name already exists in phonebook'
-    })
-  }
-
-  const person = {
-    id: generateId(),
+  const person = new Person({
     name: body.name,
     number: body.number
-  }
+  })
 
-  persons = persons.concat(person);
-
-  res.json(person)
+  person.save().then(savedPerson => {
+    res.json(savedPerson)
+  })
 })
 
 app.get('/api/persons/:id', (req, res) => {
@@ -107,7 +67,6 @@ app.delete('/api/persons/:id', (req, res) => {
 
   res.status(204).end()
 })
-
 
 // Start App
 const PORT = process.env.PORT
