cf7e0a50b3a2d6c3d0be5c02f36b6f03763dffbf
Alex Koenig <alex.koenig4@gmail.com>
2021-10-02 13:39:33 -0400

    Exercise 3.11

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ morgan.token('body', (req, res) => {
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('build'))
 
 app.get('/info', (req, res) => {
   const date = new Date();
